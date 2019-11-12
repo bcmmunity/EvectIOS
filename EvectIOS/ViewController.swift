753a6a8afa0e9af12a9e11c2882cd693bd93e010
0nly1 <modernwolf2000@gmail.com>
2019-11-12 12:49:27 +0300

    Initial Commit

---
+++ EvectIOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EvectIOS
+//
+//  Created by Modern Wolf on 12/11/2019.
+//  Copyright © 2019 Zakurdaev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
